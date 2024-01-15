28daee965233e0719c1525781ba14381a5cc914b
operezcc <142538269+operezcc@users.noreply.github.com>
2024-01-15 10:56:19 -0600

    Merging develop to master

--- Ejercicio28.dart
+++ Ejercicio28.dart
@@ -18,3 +18,32 @@ Si la cifra es de 4 digitos, se eleva a 4, y si es de 5 a 5.
 Ejemplos:
 esArmstrong(371)  // Devuelve: true
 */
+
+import 'dart:math';
+
+class Number {
+  static bool isArmstrong(int number){
+    int originalNumber = number;
+    // print(originalNumber);
+    List<String> numberString = [];
+    var numberInt = [];
+    int sum = 0;
+    // bool result;
+    numberString = number.toString().split('');
+    for(int i = 0; i < numberString.length; i++){
+      numberInt.add( pow(int.parse(numberString[i]), 3) );
+      // print(numberInt[i]);
+    }
+    for(number in numberInt){
+      sum += number;
+    }
+    // print('sum: ${sum} - number: ${number}');
+    // result = sum == originalNumber;
+    // print(result);
+    return sum == originalNumber;
+  }
+}
+
+void main(){
+  print(Number.isArmstrong(371));
+}
