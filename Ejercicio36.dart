826c3ab70014e65aeda301d676658b1f6429e2b6
operezcc <142538269+operezcc@users.noreply.github.com>
2024-01-24 14:34:30 -0600

    Adding ejercicio 36

--- Ejercicio36.dart
+++ Ejercicio36.dart
@@ -6,57 +6,35 @@ Advertencias:
 - Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
 - Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
 - Hay muchas soluciones validas para un mismo ejercicio.
- 
+
 Enunciado Ejercicio 36:
 Crea un programa que encuentre las parejas de números que como resultado
 dan un número que pasamos a la función por parametro.
- 
+
 Ejemplos:
 encontrarParejasConSuma([1, 2, 3, 4], 5);
- 
-Devuelve: 
+
+Devuelve:
 [ [ 1, 4 ], [ 2, 3 ] ]
- 
+
 */
 
-class Couples {
-  static List<List<int>> findCouplesWithAddition( List<int> numbers, int expectedResult ){
-    List<List<int>> result = [];
-    // List<List<int>> finalResult = [];
-    // List<int> copyOfNumbers = numbers;
-    for(int i = 0; i < numbers.length; i++){
-      var firstNumber = numbers[i];
-      var secondNumber = expectedResult - firstNumber;
 
-      if(numbers.contains(secondNumber) && secondNumber != firstNumber){
-        result.add([firstNumber, secondNumber]);
+
+class Couples {
+  static List<List<int>> findCouplesWithSum(List<int> numbers, int expectedResult) {
+    List<List<int>> result = [];
+    for(int i = 0; i < numbers.length; i++){
+      for(int j = i + 1; j < numbers.length; j++){
+        if(numbers[i] + numbers[j] == expectedResult){
+          result.add([numbers[i], numbers[j]]);
+        }
       }
     }
-
-    // for(int i = 0; i < numbers.length; i++){
-    //   result[i] = [];
-    //   if(result[i] != []){
-    //     finalResult.add(result[i]);
-    //   }
-    // }
-    
     return result;
   }
 }
 
-void main(){
-  print(Couples.findCouplesWithAddition([1,2,3,4], 5));
+void main() {
+  print( Couples.findCouplesWithSum([1, 2, 3, 4,5,6], 7) );
 }
-
-// EXAMPLE
-// void main() {
-//   List<int> array = [1, 2, 3, 4, 5];
-//   List<List<int>> arregloDeArreglos = [];
-
-//   for (int i = 0; i < array.length; i++) {
-//     List<int> subArray = [array[i]];
-//     arregloDeArreglos.add(subArray);
-//   }
-
-//   print(arregloDeArreglos); // [[1], [2], [3], [4], [5]]
-// }
