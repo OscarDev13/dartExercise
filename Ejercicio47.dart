0a8572a9fe1dfd8aa7b8fd93fd7dddd14b09ea42
operezcc <142538269+operezcc@users.noreply.github.com>
2024-02-19 11:49:17 -0600

    Finish Ejercicio 47

--- Ejercicio47.dart
+++ Ejercicio47.dart
@@ -26,7 +26,7 @@ quienGana([["PIEDRA", "TIJERA"], ["TIJERA", "PAPEL"], ["PAPEL", "PIEDRA"]]);
 
 class Game {
   String whoIsWinner( List<List<String>> game){
-    const options = ['PIEDRA', 'TIJERA', 'TIJERA'];
+    const options = ['PIEDRA', 'PAPEL', 'TIJERA'];
     String player1, player2, result='';
     int countPlayer1=0, countPlayer2=0;
     // print(game);
@@ -34,25 +34,37 @@ class Game {
     // print(game[0][0]);
 
     for(int i = 0; i < game.length; i++){
-      player1 = game[i][0];
-      player2 = game[i][1];
-      print('$player1 $player2');
+      player1 = game[i][0].toLowerCase();
+      player2 = game[i][1].toLowerCase();
+      // print('$player1 $player2');
       // Evaluate cases when both players are the winners
-      if(player1 == options[0].toLowerCase() && player2 == options[0].toLowerCase()){
+      if((player1 == options[0].toLowerCase() && player2 == options[0].toLowerCase()) || (player1 == options[1].toLowerCase() && player2 == options[1].toLowerCase()) || (player1 == options[2].toLowerCase() && player2 == options[2].toLowerCase())){
         countPlayer1++;
         countPlayer2++;
       }
       // Evaluates cases when palyer1 is the winner
-      // if(player1 == )
-      print('$countPlayer1 $countPlayer2');
-
+      else if((player1 == options[0].toLowerCase() && player2 == options[2].toLowerCase()) || (player1 == options[1].toLowerCase() && player2 == options[0].toLowerCase()) || (player1 == options[2].toLowerCase() && player2 == options[1].toLowerCase()) ){
+        // print('$countPlayer1 $countPlayer2');
+        countPlayer1++;
+      }
+      else if((player2 == options[0].toLowerCase() && player1 == options[2].toLowerCase()) || (player2 == options[1].toLowerCase() && player1 == options[0].toLowerCase()) || (player2 == options[2].toLowerCase() && player1 == options[1].toLowerCase()) ){
+        countPlayer2++;
+      }
+      else {
+        print('falta algun caso sin evaluar');
+      }
+    }
       //Evaluate cases when player 2 is the winner
-
       // Evaluate who is the final winner --> countplayer1 > countplayer2, countplayer2 > countplayer1, countplayer1 == countplayer2
       if(countPlayer1 == 3 && countPlayer2 == 3){
         result = 'Empate';
+      }else if(countPlayer1 > countPlayer2){
+        result = 'Jugador 1';
+      }else if(countPlayer2 > countPlayer1){
+        result = 'Jugador 2';
+      }else {
+        result = 'algo salio mal nunca deberia caer aqui';
       }
-    }
 
     return result;
   }
@@ -60,5 +72,7 @@ class Game {
 
 void main(){
   Game game = Game();
-  print(game.whoIsWinner([['piedra','piedra'],['piedra','piedra'], ['piedra','piedra']]));
+  print(game.whoIsWinner([["piedra", "tijera"], ["tijera", "piedra"], ["papel", "tijera"]])); // Player 2
+  print(game.whoIsWinner([["piedra", "piedra"], ["tijera", "tijera"], ["papel", "papel"]])); // Both
+  print(game.whoIsWinner([["piedra", "tijera"], ["tijera", "papel"], ["PAPEL", "piedra"]])); // Player 1
 }
